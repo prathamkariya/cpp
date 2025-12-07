7aa039877d707aa93676d2a8b895af04a7ea231a
prathamkariya <pratham.kariya1@gmail.com>
2025-12-07 14:43:53 +0530

    Remove commented-out code and clean up formatting in Hello World program

--- helloworld.cpp
+++ helloworld.cpp
@@ -1,7 +1,18 @@
+
 #include <iostream>
+using namespace std;
+
+int main() {
+  cout << "Hello World!";
+  return 0;
+}
+
+
+/*#include <iostream>
 int main() {
     std::cout<<"Hello World"<<std::endl;
     return 0;
     
 
 }
+*/
